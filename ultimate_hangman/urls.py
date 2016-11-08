546d9954b1c08ef70b89c65b5bb7019652cb063a
Bruno Rangel Huerta <bruno14.rangel@gmail.com>
2016-11-08 23:37:49 +0000

    Added css files

--- ultimate_hangman/urls.py
+++ ultimate_hangman/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^hang/', include('hang.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
